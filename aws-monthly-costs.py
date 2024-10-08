30b4203ce5a8c25d83cb3901d97b7bdd5d4997df
whoDoneItAgain <12127434+whoDoneItAgain@users.noreply.github.com>
2024-10-08 10:19:41 -0400

    refactor top accounts into main function

--- aws-monthly-costs.py
+++ aws-monthly-costs.py
@@ -17,7 +17,14 @@ LOGGER = logging.getLogger("amc")
 DEFAULT_OUTPUT_FOLDER = "./outputs/"
 DEFAULT_OUTPUT_PREFIX = "aws-monthly-costs"
 
-VALID_RUN_MODES = ["bu", "bu-daily", "service", "service-daily"]
+VALID_RUN_MODES = [
+    "account",
+    "account-daily",
+    "bu",
+    "bu-daily",
+    "service",
+    "service-daily",
+]
 
 
 def get_config_args():
@@ -96,6 +103,41 @@ def configure_logging(debug_logging: bool = False, info_logging: bool = False):
     LOGGER.addHandler(ch)
 
 
+def build_account_costs_by_account(cost_and_usage, account_list, daily_average=False):
+    account_costs: dict = {}
+    account_name_list: list = []
+
+    if daily_average:
+        this_year = (date.today()).year
+
+    for period in cost_and_usage["ResultsByTime"]:
+        month_costs: dict = {}
+        cost_month = datetime.strptime(period["TimePeriod"]["Start"], "%Y-%m-%d")
+        cost_month_name = cost_month.strftime("%b")
+
+        if daily_average:
+            day_count = calendar.monthrange(this_year, cost_month.month)[1]
+
+        for account in period["Groups"]:
+            for org_account in account_list:
+                if account["Keys"][0] == org_account["Id"]:
+                    if daily_average:
+                        month_costs[org_account["Name"]] = (
+                            float(account["Metrics"]["UnblendedCost"]["Amount"])
+                            / day_count
+                        )
+                    else:
+                        month_costs[org_account["Name"]] = account["Metrics"][
+                            "UnblendedCost"
+                        ]["Amount"]
+                    if account not in account_name_list:
+                        account_name_list.append(org_account["Name"])
+
+        account_costs[cost_month_name] = month_costs
+
+    return account_costs, account_name_list
+
+
 def build_account_costs_by_bu(cost_and_usage, daily_average=False):
     account_costs: dict = {}
 
@@ -156,6 +198,24 @@ def build_costs_by_service(cost_and_usage, daily_average=False):
     service_list = list(set(service_list))
 
     return service_costs, service_list
+
+
+def build_cost_matrix_by_account(account_costs, account_name_list):
+    cost_matrix: dict = {}
+
+    for cost_month, costs_for_month in account_costs.items():
+        account_month_costs: dict = {}
+
+        for account_name in account_name_list:
+            if account_name in costs_for_month:
+                account_month_costs[account_name] = float(costs_for_month[account_name])
+        for k in account_month_costs:
+            account_month_costs[k] = round(account_month_costs[k], 2)
+        account_month_costs["total"] = sum(account_month_costs.values())
+
+        cost_matrix[cost_month] = account_month_costs
+
+    return cost_matrix
 
 
 def build_cost_matrix_by_bu(
@@ -233,6 +293,74 @@ def build_cost_matrix_by_service(service_list, service_costs, service_aggregatio
         cost_matrix[cost_month] = service_month_costs
 
     return cost_matrix
+
+
+def monthly_costs_by_account(
+    ce_client,
+    o_client,
+    start_date,
+    end_date,
+    top_cost_count,
+    daily_average=False,
+):
+    account_list: list = []
+    list_accounts_response = o_client.list_accounts()
+    account_list.extend(list_accounts_response["Accounts"])
+
+    while "NextToken" in list_accounts_response:
+        list_accounts_response = o_client.list_accounts(
+            NextToken=list_accounts_response["NextToken"]
+        )
+        account_list.extend(list_accounts_response["Accounts"])
+
+    account_get_cost_and_usage = ce_client.get_cost_and_usage(
+        TimePeriod={
+            "Start": start_date.strftime("%Y-%m-%d"),
+            "End": end_date.strftime("%Y-%m-%d"),
+        },
+        Granularity="MONTHLY",
+        Metrics=["UnblendedCost"],
+        GroupBy=[{"Type": "DIMENSION", "Key": "LINKED_ACCOUNT"}],
+    )
+
+    LOGGER.debug(account_get_cost_and_usage["ResultsByTime"])
+
+    account_costs, account_name_list = build_account_costs_by_account(
+        account_get_cost_and_usage,
+        account_list,
+        daily_average,
+    )
+
+    LOGGER.debug(account_costs)
+
+    account_cost_matrix = build_cost_matrix_by_account(account_costs, account_name_list)
+
+    recent_month = (list(account_cost_matrix.items())[-1])[0]
+
+    recent_month_costs = account_cost_matrix[recent_month]
+
+    recent_month_costs_sorted = dict(
+        sorted(recent_month_costs.items(), key=lambda item: item[1], reverse=True)
+    )
+
+    sorted_account_cost_matrix = {}
+
+    sorted_accounts = list(recent_month_costs_sorted.keys())
+
+    top_sorted_accounts = sorted_accounts[0:top_cost_count]
+
+    for cost_month in account_cost_matrix.keys():
+        top_services_month_total: float = 0
+        month_cost: dict = {}
+        for service in top_sorted_accounts:
+            if service != "total":
+                month_cost[service] = account_cost_matrix[cost_month][service]
+                top_services_month_total += account_cost_matrix[cost_month][service]
+        month_cost["total"] = round(top_services_month_total, 2)
+
+        sorted_account_cost_matrix[cost_month] = month_cost
+
+    return sorted_account_cost_matrix
 
 
 def monthly_costs_by_bu(
@@ -379,7 +507,14 @@ def export_report(export_file, cost_matrix, group_list, group_by_type):
 
         months = list(cost_matrix.keys())
 
-        if group_by_type == "bu":
+        if group_by_type == "account":
+            for account in group_list:
+                csv_row: list = []
+                csv_row.append(account)
+                for month in months:
+                    csv_row.append(cost_matrix[month][account])
+                writer.writerow(csv_row)
+        elif group_by_type == "bu":
             bus = list(group_list.keys())
             bus.remove("ss")
             bus.extend(["total"])
@@ -397,6 +532,24 @@ def export_report(export_file, cost_matrix, group_list, group_by_type):
                     if service in cost_matrix[month]:
                         csv_row.append(cost_matrix[month][service])
                 writer.writerow(csv_row)
+
+
+def build_account_names(cost_matrix):
+    account_names: list = []
+
+    months = list(cost_matrix.keys())
+
+    current_month = months[-1]
+
+    account_names.extend(cost_matrix[current_month])
+    months.pop()
+
+    for month in months:
+        for account_name in cost_matrix[month].keys():
+            if account_name not in account_names:
+                account_names.append(account_name)
+
+    return account_names
 
 
 def build_service_list_with_aggregation(cost_matrix, service_aggregation):
@@ -477,12 +630,45 @@ def main():
 
     aws_session = boto3.Session(profile_name=aws_profile)
     ce_client = aws_session.client("ce")
+    if any(x in ["account", "account-daily"] for x in run_modes):
+        o_client = aws_session.client("organizations")
 
     for run_mode in run_modes:
         export_file = Path(
             f"{DEFAULT_OUTPUT_FOLDER}{DEFAULT_OUTPUT_PREFIX}-{run_mode}.csv"
         ).absolute()
         match run_mode:
+            # by Account
+            case "account":
+                cost_matrix = monthly_costs_by_account(
+                    ce_client,
+                    o_client,
+                    start_date,
+                    end_date,
+                    top_costs_counts["account"] + 1,
+                )
+
+                account_names = build_account_names(cost_matrix)
+
+                export_report(
+                    export_file, cost_matrix, account_names, group_by_type="account"
+                )
+            case "account-daily":
+                cost_matrix = monthly_costs_by_account(
+                    ce_client,
+                    o_client,
+                    start_date,
+                    end_date,
+                    top_costs_counts["account"] + 1,
+                    daily_average=True,
+                )
+                export_report(
+                    export_file,
+                    cost_matrix,
+                    account_names,
+                    group_by_type="account",
+                )
+            # by Bu
             case "bu":
                 cost_matrix = monthly_costs_by_bu(
                     ce_client,
@@ -506,6 +692,7 @@ def main():
                 export_report(
                     export_file, cost_matrix, account_list, group_by_type="bu"
                 )
+            # by Service
             case "service":
                 cost_matrix = monthly_costs_by_service(
                     ce_client,
@@ -522,7 +709,6 @@ def main():
                 export_report(
                     export_file, cost_matrix, service_list_agg, group_by_type="service"
                 )
-
             case "service-daily":
                 cost_matrix = monthly_costs_by_service(
                     ce_client,
